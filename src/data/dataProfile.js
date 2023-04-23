e597808ccee296f6826750f9de26448365b64e53
Lucas G <lucasg.devmail93@gmail.com>
2023-04-23 14:52:53 -0300

    feat: profile section portrait mobile finished

--- src/data/dataProfile.js
+++ src/data/dataProfile.js
@@ -3,7 +3,6 @@ export default {
   email: "lucaswx.android@gmail.com",
   linkedin: "https://www.linkedin.com/in/lucas-g-oliveira/",
   gitHub: "https://github.com/lucas-g-oliveira/",
-  role: "Desenvolvedor Web FullStack",
-  aboutMe: "Código, Café e Rock rool",
-  experiences: ""
+  role: "Dev Web FullStack",
+  aboutMe: "Formado em Análise e Desenvolvimento de Sistemas pela Estácio e concluindo formação em Desenvolvimento Web FullStack pela Trybe, exponho aqui alguns projetos realizados no ultimo ano, que comprovam algumas das minhas hardskills. Fique á vontade para conhecer meu linkedin e github também.",
 }
